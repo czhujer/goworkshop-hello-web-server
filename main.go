6c17ae70acba3deaf527b686183126ec8d35b3ba
Patrik Majer <patrik.majer.pisek@gmail.com>
2019-11-01 15:10:50 +0100

    add simple http router

--- main.go
+++ main.go
@@ -8,10 +8,29 @@ import (
 type Greater struct {
 }
 
+type Router struct {
+	greater *Greater
+}
+
 func (g *Greater) Hello(rw http.ResponseWriter, req *http.Request) {
 	rw.WriteHeader(http.StatusTeapot)
 	rw.Write([]byte("I'm teapot\n"))
+}
 
+func (g *Greater) Bye(rw http.ResponseWriter, req *http.Request) {
+	//rw.WriteHeader(http.StatusTeapot)
+	rw.Write([]byte("Bye\n"))
+}
+
+func (r *Router) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
+	switch req.URL.Path {
+	case "/hello":
+		r.greater.Hello(rw,req)
+	case "/bye":
+		r.greater.Bye(rw, req)
+	default:
+		http.NotFound(rw, req)
+	}
 }
 
 func main() {
@@ -21,8 +40,15 @@ func main() {
 `
 	greater := &Greater{}
 
+	router := &Router{
+		greater: greater,
+	}
+
 	fmt.Println("Starting web server..")
 	fmt.Println(dottedstring)
 
-	http.ListenAndServe(":3000", http.HandlerFunc(greater.Hello))
+	err := http.ListenAndServe(":3000", router)
+	if err != nil {
+		panic(err)
+	}
 }
